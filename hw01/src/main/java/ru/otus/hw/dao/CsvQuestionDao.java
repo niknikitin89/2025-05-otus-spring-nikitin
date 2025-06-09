26d87604f372b44d4ebff6d94f081e47f9b0962c
Nik <niknikitin89@yandex.ru>
2025-06-09 13:13:47 +0300

    Причесал константы и работу с исключениями

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -7,6 +7,7 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -14,32 +15,60 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
+    private static final int SKIP_LINES_NUMBER = 1;
+
+    private static final char SEPARATOR = ';';
+
+    private static final String NO_FILE_NAME = "Empty filename";
+
+    private static final String NO_FILE_IN_RESOURCE_FOLDER = "No file with questions in application resource";
+
+    private static final String ERROR_FILE_READING = "Error reading file";
+
+    private static final String NO_QUESTION_READ = "No question read";
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream resource = classLoader.getResourceAsStream(fileNameProvider.getTestFileName());
 
-        if (resource == null) {
-            throw new QuestionReadException(
-                    String.format("File access error - \"%s\"", fileNameProvider.getTestFileName()));
-        }
+        try (InputStream resource = getFileAsStream();
+             InputStreamReader reader = new InputStreamReader(resource)) {
 
-        try {
-            List<QuestionDto> questionsDto = new CsvToBeanBuilder<QuestionDto>(
-                    new InputStreamReader(resource))
-                    .withSkipLines(1)
-                    .withSeparator(';')
+            List<QuestionDto> questionsDto = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSkipLines(SKIP_LINES_NUMBER)
+                    .withSeparator(SEPARATOR)
                     .withType(QuestionDto.class)
                     .build()
                     .parse();
 
+            if (questionsDto == null || questionsDto.isEmpty()) {
+                throw new QuestionReadException(NO_QUESTION_READ);
+            }
+
             return questionsDto.stream()
                     .map(QuestionDto::toDomainObject)
                     .collect(Collectors.toList());
-        } catch (RuntimeException e) {
-            throw new QuestionReadException("Error reading file", e);
+        } catch (IOException e) {
+            throw new QuestionReadException(ERROR_FILE_READING, e);
         }
+    }
+
+    private InputStream getFileAsStream() throws IOException {
+        String filename = fileNameProvider.getTestFileName();
+
+        if (filename == null || filename.isEmpty()) {
+            throw new IOException(NO_FILE_NAME);
+        }
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream resource = classLoader.getResourceAsStream(fileNameProvider.getTestFileName());
+
+        if (resource == null) {
+            throw new IOException(NO_FILE_IN_RESOURCE_FOLDER);
+        }
+
+        return resource;
     }
 }
