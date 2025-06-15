80f71ad6c0d382e6dcdbb8b7d2c0a604dd5499bc
Nik <niknikitin89@yandex.ru>
2025-06-15 19:35:18 +0300

    It's work!

--- hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,79 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Repository
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+    private static final int SKIP_LINES_NUMBER = 1;
+
+    private static final char SEPARATOR = ';';
+
+    private static final String NO_FILE_NAME = "Empty filename";
+
+    private static final String NO_FILE_IN_RESOURCE_FOLDER = "No file with questions in application resource";
+
+    private static final String ERROR_FILE_READING = "Error reading file";
+
+    private static final String NO_QUESTION_READ = "No question read";
+
+    private static final String ERROR_CSV_READ = "Error reading csv file";
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        try (InputStream resource = getFileAsStream();
+             InputStreamReader reader = new InputStreamReader(resource)) {
+
+            List<QuestionDto> questionsDto = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSkipLines(SKIP_LINES_NUMBER)
+                    .withSeparator(SEPARATOR)
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse();
+
+            if (questionsDto == null || questionsDto.isEmpty()) {
+                throw new QuestionReadException(NO_QUESTION_READ);
+            }
+
+            return questionsDto.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new QuestionReadException(ERROR_FILE_READING, e);
+        } catch (RuntimeException e) {
+            throw new QuestionReadException(ERROR_CSV_READ, e);
+        }
+    }
+
+    private InputStream getFileAsStream() throws IOException {
+        String filename = fileNameProvider.getTestFileName();
+
+        if (filename == null || filename.isEmpty()) {
+            throw new IOException(NO_FILE_NAME);
+        }
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream resource = classLoader.getResourceAsStream(fileNameProvider.getTestFileName());
+
+        if (resource == null) {
+            throw new IOException(NO_FILE_IN_RESOURCE_FOLDER);
+        }
+
+        return resource;
     }
 }
--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+@Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
@@ -19,10 +22,26 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (var question : questions) {
+            var isAnswerValid = askQuestion(question); // Задать вопрос, получить ответ
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private boolean askQuestion(Question question) {
+        ioService.printLine("");
+        ioService.printFormattedLine("%s%n", question.text());
+
+        int answerNumber = 0;
+        for (var answer : question.answers()) {
+            answerNumber++;
+            ioService.printFormattedLine("%d. %s", answerNumber, answer.text());
+        }
+
+        ioService.printFormattedLine("%n%s","Enter number of correct answer:");
+        int userAnswer = ioService.readIntForRange(1, answerNumber, "Answer out of range");
+
+        return question.answers().get(userAnswer - 1).isCorrect();
     }
 }
