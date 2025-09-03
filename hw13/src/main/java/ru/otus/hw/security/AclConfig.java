73d5e0ca8ca62482b398da84d2b8e73213f6b6c1
Nik <niknikitin89@yandex.ru>
2025-09-03 23:51:41 +0300

    Пошла работа с ACL

--- hw13/src/main/java/ru/otus/hw/security/AclConfig.java
+++ hw13/src/main/java/ru/otus/hw/security/AclConfig.java
@@ -2,8 +2,9 @@ package ru.otus.hw.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.ehcache.EhCacheFactoryBean;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.cache.CacheManager;
+//import org.springframework.cache.ehcache.EhCacheFactoryBean;
+//import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
@@ -15,6 +16,7 @@ import org.springframework.security.acls.domain.AclAuthorizationStrategyImpl;
 import org.springframework.security.acls.domain.ConsoleAuditLogger;
 import org.springframework.security.acls.domain.DefaultPermissionGrantingStrategy;
 import org.springframework.security.acls.domain.EhCacheBasedAclCache;
+import org.springframework.security.acls.domain.SpringCacheBasedAclCache;
 import org.springframework.security.acls.jdbc.BasicLookupStrategy;
 import org.springframework.security.acls.jdbc.JdbcMutableAclService;
 import org.springframework.security.acls.jdbc.LookupStrategy;
@@ -31,26 +33,37 @@ public class AclConfig {
 
     private final DataSource dataSource;
 
+    private final CacheManager cacheManager;
+
+//    @Bean
+//    public EhCacheBasedAclCache aclCache() {
+//        return new EhCacheBasedAclCache(
+//                Objects.requireNonNull(aclEhCacheFactoryBean().getObject()),
+//                permissionGrantingStrategy(),
+//                aclAuthorizationStrategy()
+//        );
+//    }
+//
+//    @Bean
+//    public EhCacheFactoryBean aclEhCacheFactoryBean() {
+//        EhCacheFactoryBean ehCacheFactoryBean = new EhCacheFactoryBean();
+//        ehCacheFactoryBean.setCacheManager(aclCacheManager().getObject());
+//        ehCacheFactoryBean.setCacheName("aclCache");
+//        return ehCacheFactoryBean;
+//    }
+//
+//    @Bean
+//    public EhCacheManagerFactoryBean aclCacheManager() {
+//        return new EhCacheManagerFactoryBean();
+//    }
+
     @Bean
-    public EhCacheBasedAclCache aclCache() {
-        return new EhCacheBasedAclCache(
-                Objects.requireNonNull(aclEhCacheFactoryBean().getObject()),
+    public SpringCacheBasedAclCache aclCache() {
+        return new SpringCacheBasedAclCache(
+                cacheManager.getCache("aclCache"),
                 permissionGrantingStrategy(),
                 aclAuthorizationStrategy()
         );
-    }
-
-    @Bean
-    public EhCacheFactoryBean aclEhCacheFactoryBean() {
-        EhCacheFactoryBean ehCacheFactoryBean = new EhCacheFactoryBean();
-        ehCacheFactoryBean.setCacheManager(aclCacheManager().getObject());
-        ehCacheFactoryBean.setCacheName("aclCache");
-        return ehCacheFactoryBean;
-    }
-
-    @Bean
-    public EhCacheManagerFactoryBean aclCacheManager() {
-        return new EhCacheManagerFactoryBean();
     }
 
     @Bean
@@ -80,8 +93,8 @@ public class AclConfig {
 
     @Bean
     public LookupStrategy lookupStrategy() {
-//        return new BasicLookupStrategy(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
-        return new CustomLookupStrategy(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
+        return new BasicLookupStrategy(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
+//        return new CustomLookupStrategy(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
     }
 
     @Bean
