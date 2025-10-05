63ff311d3eec80386352fc00c4e6d0c1584a6870
Nik <niknikitin89@yandex.ru>
2025-10-05 21:33:12 +0300

    Готово

---
+++ hw17/src/main/java/ru/otus/hw/dto/CommentaryDto.java
@@ -0,0 +1,34 @@
+package ru.otus.hw.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.otus.hw.models.Commentary;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentaryDto {
+
+    private long id;
+
+    @NotBlank(message = "Empty Text")
+    private String text;
+
+    private BookDto book;
+
+    public static CommentaryDto fromDomainObjectWithBook(Commentary commet) {
+        var book = BookDto.fromDomainObject(commet.getBook());
+        return new CommentaryDto(commet.getId(), commet.getText(), book);
+    }
+
+    public static CommentaryDto fromDomainObject(Commentary commet) {
+        return new CommentaryDto(commet.getId(), commet.getText(), null);
+    }
+
+    public Commentary toDomainObject() {
+        return new Commentary(id, text, book.toDomainObject());
+    }
+
+}
