9797b6a1eba8a71f4fdea3ad09f7e0e0134b7dd3
Nik <niknikitin89@yandex.ru>
2025-10-13 19:16:02 +0300

    Скопировал ДЗ 10.

---
+++ hw18/src/main/java/ru/otus/hw/models/Commentary.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "commentaries")
+public class Commentary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private long id;
+
+    @Column(name = "text")
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private String text;
+
+    @ManyToOne(targetEntity = Book.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
